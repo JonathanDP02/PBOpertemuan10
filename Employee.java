782ea7f2d9520a73161bf3cf0ffcdbd2af8eaf5d
JonathanDP02 <jonatanpangestu23@gmail.com>
2025-11-02 12:14:29 +0700

    qwqw

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    protected String name;
+
+    public String getEmployeeinfo(){
+        return "Name = "+name;
+    }
+
+    public String getEmployeeInfo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEmployeeInfo'");
+    }
+}
---
+++ Plant.java
@@ -0,0 +1,10 @@
+public class Plant {
+
+    public void doDestroy(Destroyable d) {
+        if (d instanceof Zombie) {
+            ((Zombie) d).destroyed();
+        } else if (d instanceof Barrier) {
+            ((Barrier) d).destroyed();
+        }
+    }
+}
